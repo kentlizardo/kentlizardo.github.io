254365abfec5a1c4a76fca5bb3114328979cee6d
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-02-15 05:41:04 -0600

    Added svelte-preprocess, adapter-static, sass, and updated copyright.

--- README.md
+++ README.md
@@ -36,3 +36,5 @@ npm run build
 You can preview the production build with `npm run preview`.
 
 > To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
+
+Contents and design of this site are © Copyright, 2024, Kent Brian Lizardo. All rights reserved.
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
+import static_adapter from '@sveltejs/adapter-static';
+import sveltePreprocess from 'svelte-preprocess';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: static_adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
+		},
+		prerender: {
+			entries: ['*']
+		}
+	},
+	preprocess: [sveltePreprocess()],
 };
 
 export default config;
