63f513edd33f155cbca224c5ef65d87df74f5c1c
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-02-20 21:41:58 -0600

    Added header suffix sprite

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,9 +1,6 @@
 <script>
-	import { page } from '$app/stores';
 	import NavBar from '$lib/components/NavBar.svelte';
 	import NavPath from '$lib/components/NavPath.svelte';
-	let path = $page.url.host;
-	const allowed_hosts = ['kentmakes.games', 'kentlizardo.github.io'];
 
 	/** @type {string} */
 	let selected_route = '';
@@ -11,18 +8,11 @@
 
 <div class="header">
 	<div class="header-logo">
-		<h1 class="oxygen-mono-regular">
-			{#if $page.url.host != 'sveltekit-prerender'}
-				{#if allowed_hosts.includes($page.url.host)}
-					{path}
-				{:else}
-					dev({path})
-				{/if}
-			{/if}
-		</h1>
-		<div class="subheader">
-			<NavPath typed_pathname={selected_route}></NavPath>
-		</div>
+		<h1 class="oxygen-mono-regular">kentmakes.games</h1>
+		<div class="header-icon"></div>
+	</div>
+	<div class="subheader">
+		<NavPath typed_pathname={selected_route}></NavPath>
 	</div>
 	<div class="navbar-container">
 		<NavBar bind:selected_route></NavBar>
@@ -36,25 +26,32 @@
 		flex-wrap: wrap;
 		align-items: center;
 	}
-
-	.navbar-container {
-		margin: 0;
-		margin-left: auto;
-	}
-
 	.header-logo {
 		display: flex;
 		flex-wrap: nowrap;
+		align-items: center;
 	}
 	.header-logo > h1 {
 		margin: 0;
 		padding: 0;
 	}
-
+	.header-icon {
+		width: 32px;
+		height: 32px;
+		background-image: url($lib/images/pfl-icons.png);
+		background-position: 0px 0px;
+		image-rendering: crisp-edges;
+		background-size: 400%;
+		filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.401));
+	}
 	.subheader {
 		/* position: relative; */
 		top: 0%;
 		left: 0%;
 		align-self: center;
+	}
+	.navbar-container {
+		margin: 0;
+		margin-left: auto;
 	}
 </style>
