dfd557659e3b5844fed06d491acaaf7cc0e76e85
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-02-23 03:26:22 -0600

    Replaced redirect in Contact to JS function.

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,6 +3,44 @@
 
 	/** @type {HTMLFormElement}*/
 	let form;
+	/** @type {HTMLElement}*/
+	let result;
+
+	function submitForm(/** @type {SubmitEvent} */ e) {
+		e.preventDefault();
+		const formData = new FormData(form);
+		const object = Object.fromEntries(formData);
+		const json = JSON.stringify(object);
+		result.innerHTML = 'Please wait...';
+
+		fetch('https://api.web3forms.com/submit', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+				'Accept': 'application/json'
+			},
+			body: json
+		})
+			.then(async (response) => {
+				let json = await response.json();
+				if (response.status == 200) {
+					result.innerHTML = json.message;
+				} else {
+					console.log(response);
+					result.innerHTML = json.message;
+				}
+			})
+			.catch((error) => {
+				console.log(error);
+				result.innerHTML = 'Something went wrong!';
+			})
+			.then(function () {
+				form.reset();
+				setTimeout(() => {
+					result.style.display = 'none';
+				}, 3000);
+			});
+	}
 
 	onMount(() => {
 		form.reset();
@@ -11,7 +49,12 @@
 
 <div>
 	<script src="https://web3forms.com/client/script.js" async defer></script>
-	<form bind:this={form} action="https://api.web3forms.com/submit" method="POST">
+	<form
+		bind:this={form}
+		action="https://api.web3forms.com/submit"
+		method="POST"
+		on:submit={submitForm}
+	>
 		<input type="hidden" name="access_key" value="9d550562-e74a-4069-8515-f719038f2d77" />
 		<input type="hidden" name="subject" value="Portfolio Contact Submission" />
 
@@ -21,6 +64,7 @@
 		<div class="h-captcha" data-captcha="true"></div>
 		<button type="submit">Submit Form</button>
 	</form>
+	<div bind:this={result}></div>
 </div>
 
 <style>
