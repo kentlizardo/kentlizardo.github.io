14863e4cba95a7f61bba861b0e82dd1757cd5ab9
Kent Lizardo <kentbrianlizardo@gmail.com>
2024-02-23 03:37:14 -0600

    Used Web3 recommended svelte example to update

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -3,44 +3,28 @@
 
 	/** @type {HTMLFormElement}*/
 	let form;
-	/** @type {HTMLElement}*/
-	let result;
 
-	function submitForm(/** @type {SubmitEvent} */ e) {
-		e.preventDefault();
+	let status = '';
+
+	const submitForm = async (/** @type {SubmitEvent} */ e) => {
+		status = 'Submitting...';
 		const formData = new FormData(form);
 		const object = Object.fromEntries(formData);
 		const json = JSON.stringify(object);
-		result.innerHTML = 'Please wait...';
-
-		fetch('https://api.web3forms.com/submit', {
+		const response = await fetch('https://api.web3forms.com/submit', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json',
 				'Accept': 'application/json'
 			},
 			body: json
-		})
-			.then(async (response) => {
-				let json = await response.json();
-				if (response.status == 200) {
-					result.innerHTML = json.message;
-				} else {
-					console.log(response);
-					result.innerHTML = json.message;
-				}
-			})
-			.catch((error) => {
-				console.log(error);
-				result.innerHTML = 'Something went wrong!';
-			})
-			.then(function () {
-				form.reset();
-				setTimeout(() => {
-					result.style.display = 'none';
-				}, 3000);
-			});
-	}
+		});
+		const result = await response.json();
+		if (result.success) {
+			console.log(result);
+			status = result.message || 'Success';
+		}
+	};
 
 	onMount(() => {
 		form.reset();
@@ -53,18 +37,18 @@
 		bind:this={form}
 		action="https://api.web3forms.com/submit"
 		method="POST"
-		on:submit={submitForm}
+		on:submit|preventDefault={submitForm}
 	>
 		<input type="hidden" name="access_key" value="9d550562-e74a-4069-8515-f719038f2d77" />
 		<input type="hidden" name="subject" value="Portfolio Contact Submission" />
 
 		<input type="text" name="name" required />
 		<input type="email" name="email" required />
-		<textarea name="message" required></textarea>
+		<textarea name="message" required rows="3"></textarea>
 		<div class="h-captcha" data-captcha="true"></div>
-		<button type="submit">Submit Form</button>
+		<input type="submit" />
 	</form>
-	<div bind:this={result}></div>
+	<div>{status}</div>
 </div>
 
 <style>
