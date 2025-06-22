c8f50179fafaa21c6fcb7963c8a5f1c57ccd4c83
Lizardo <122572067+kentlizardo@users.noreply.github.com>
2025-06-22 08:11:24 +0000

    fix: fix gallery masonry

--- assets/js/gallery-masonry-init.js
+++ assets/js/gallery-masonry-init.js
@@ -1,13 +1,17 @@
-var masonry = new MiniMasonry({
+const masonry = new MiniMasonry({
   container: '#mediapost-list',
   baseWidth: 300,
   gutterX: 20,
-  gutterY: 20,
+  gutterY: 20
 });
 
-const updateMasonry = function(instance) {
+const updateMasonry = function (instance, image) {
   masonry.layout();
 };
 
-imagesLoaded('#mediapost-list', { background: true }, updateMasonry);
+var imgLoad = imagesLoaded(
+  '#mediapost-list',
+  { background: true },
+  updateMasonry
+);
 imgLoad.on('progress', updateMasonry);
