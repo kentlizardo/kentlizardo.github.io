fbff6b3660e8c70ee71c412c136ad20ebd5ba557
Lizardo <122572067+kentlizardo@users.noreply.github.com>
2025-06-30 22:04:40 +0000

    feat: make compound nav header sticky
    Few other changes include:
    - Cachebust js distribution links based on content
    - Link blog to same minified js as home

--- _javascript/home.js
+++ _javascript/home.js
@@ -1,8 +1,9 @@
 import { basic, initSidebar, initTopbar } from './modules/layouts';
-import { initLocaleDatetime, loadImg } from './modules/components';
+import { initLocaleDatetime, loadImg, listenStuck } from './modules/components';
 
 loadImg();
 initLocaleDatetime();
 initSidebar();
 initTopbar();
 basic();
+listenStuck();
